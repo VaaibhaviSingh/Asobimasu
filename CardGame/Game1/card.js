8f874464ef380e00e4d652423da017f850935e76
Vaaibhavi Singh <vaaibhavisingh@gmail.com>
2018-03-17 17:02:24 +0530

    Add initial things

---
+++ CardGame/Game1/card.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('#img1').on('click',function(){
+    console.log("This is card 1");
+  });
+  $('#img2').on('click',function(){
+    console.log("This is card 2");
+  });
+  $('#img3').on('click',function(){
+    console.log("This is card 3");
+  });
+})
